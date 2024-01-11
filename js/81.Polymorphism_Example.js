7028fb43af71a75d678149aceed3a5135d2c4e22
Fahd Ogunnaike <realabiolafahad@gmail.com>
2024-01-11 15:25:20 +0100

    polymorphism

--- js/81.Polymorphism_Example.js
+++ js/81.Polymorphism_Example.js
@@ -1,3 +1,45 @@
 {
+    {
+        let User =
+        {
+            active: false,
+            sayTesleem: function()
+            {
+                return this.name + " says assalamu alaikum";
+            }
+        }
     
+        let student =
+        {
+            name: 'ibn abbas',
+            studies: ['mechanics', 'AI', 'data science']
+        }
+
+        let corper =
+        {
+            name: 'abu bakr',
+            teaches: ['physics', 'robotics'],
+            sayTesleem: function()
+            {
+                let message = this.name + " teaches ";
+                this.teaches.forEach(function(e)
+                {
+                    message += e + " ";
+                });
+                return message;
+            }
+        };
+    
+        Object.setPrototypeOf(student, User);
+        Object.setPrototypeOf(corper, User);
+
+        corper.active = true;
+    
+        let newMembers = [corper, student];
+
+        newMembers.forEach(function(e) 
+        {
+            console.log(e.sayTesleem());
+        });
+    }
 }
