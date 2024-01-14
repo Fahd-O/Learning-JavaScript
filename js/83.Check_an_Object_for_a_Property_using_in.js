495d0f66d6567939ac53ab3d5acd501d56bfa5d7
Fahd Ogunnaike <realabiolafahad@gmail.com>
2024-01-14 15:01:16 +0100

    83.Check an Object for a Property using in. deleted unneeded file as well

---
+++ js/83.Check_an_Object_for_a_Property_using_in.js
@@ -0,0 +1,52 @@
+
+{
+    let User =
+    {
+        active: false,
+        sayTesleem: function()
+        {
+            return this.name + " says assalamu alaikum";
+        }
+    }
+
+    let student =
+    {
+        name: 'ibn abbas',
+        studies: ['mechanics', 'AI', 'data science']
+    }
+
+    let corper =
+    {
+        name: 'abu bakr',
+        teaches: ['physics', 'robotics'],
+        sayTesleem: function()
+        {
+            let message = this.name + " teaches ";
+            this.teaches.forEach(function(e)
+            {
+                message += e + " ";
+            });
+            return message;
+        }
+    };
+
+    Object.setPrototypeOf(student, User);
+    Object.setPrototypeOf(corper, User);
+
+    corper.active = true;
+
+    let newMembers = [corper, student];
+
+    newMembers.forEach(function(e) 
+    {
+        console.log(e.sayTesleem());
+    });
+
+    console.log("Is name inside corper:", "named" in corper);
+
+    console.log("Is name not inside corper:", corper.named == undefined);
+
+    console.log(corper);
+}
+
+    
