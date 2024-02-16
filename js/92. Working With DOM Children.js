0d71f7e46eec1119c24403bbbb0d134026c50c81
Fahd Ogunnaike <realabiolafahad@gmail.com>
2024-02-16 21:55:59 +0100

    92. Working With DOM Children

---
+++ js/92. Working With DOM Children.js
@@ -0,0 +1,8 @@
+{
+    let list = document.childNodes[1].childNodes[2].childNodes[3];
+
+    console.log(list);
+    console.log(list.parentElement);
+    console.log(list.nextSibling);
+    console.log(list.nextSibling.nextSibling);
+}
