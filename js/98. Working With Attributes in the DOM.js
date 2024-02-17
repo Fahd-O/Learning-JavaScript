257177e6dd9c88af26e353514d68fb7df40ccdc6
Fahd Ogunnaike <realabiolafahad@gmail.com>
2024-02-17 22:33:10 +0100

    98. Working With Attributes in the DOM

---
+++ js/98. Working With Attributes in the DOM.js
@@ -0,0 +1,9 @@
+{
+    document.getElementById('putin').value = 'My name is...';
+
+    console.log(document.getElementById('putin').hasAttribute('tacos'));
+
+    console.log(document.getElementById('putin').getAttribute('tacos'));
+
+
+}
