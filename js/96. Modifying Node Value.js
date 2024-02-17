28203be08f5221a061410056cd181c927a4d958e
Fahd Ogunnaike <realabiolafahad@gmail.com>
2024-02-17 09:25:21 +0100

    96. Modifying Node Value

---
+++ js/96. Modifying Node Value.js
@@ -0,0 +1,9 @@
+{
+    let paragraphs = document.getElementsByTagName("p");
+
+    console.log(paragraphs);
+
+    console.log(paragraphs[0].childNodes[0]);
+
+    console.log(paragraphs[0].childNodes[0].childNodes[0].nodeValue= "Llama");
+}
